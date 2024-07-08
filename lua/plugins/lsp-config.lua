2e0b97ee99740b37f6ff95ec223eac30526c490c
Mehdi <mehdi.elbrahmi02@gmail.com>
2024-07-08 09:33:09 -0400

    save_1

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "clangd", "pyright"}
+    })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      --vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
