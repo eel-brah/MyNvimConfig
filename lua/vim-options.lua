48ba24a86b123140d287dda6c2e9f787cc9c0dda
El Mehdi El Brahmi <eel-brah@e3r10p17.1337.ma>
2024-10-22 15:16:40 +0100

    Update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -65,6 +65,15 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Tabs
+vim.keymap.set('n', '<leader>nt', ':tabnew<CR>')
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>')
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>')
+
+-- Splits
+vim.keymap.set('n', '<leader>sv', '<C-w>v')
+vim.keymap.set('n', '<leader>sh', '<C-w>s')
+
 -- Move between splits
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
